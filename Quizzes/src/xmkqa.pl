17aa69fc1d487e0418b547633f01082672ccf3d7
Volker Brendel <vbrendel@indiana.edu>
2016-09-07 21:18:00 -0400

    updated FMABqlib.yml and xmkqa.pl

--- Quizzes/src/xmkqa.pl
+++ Quizzes/src/xmkqa.pl
@@ -1,7 +1,7 @@
 #! /usr/bin/perl -w
 #
 # xmkqa.pl
-# Version of September 6, 2016.  Volker Brendel
+# Version of September 7, 2016.  Volker Brendel
 
 use strict; 
 use Getopt::Std;
@@ -11,17 +11,24 @@ my $subsubject = "bash";
 my $type = "explain";
 my $difficulty = "easy";
 
-my $USAGE="\nUsage: $0 [-s subject] [-r subsubject] [-t type] [-d difficulty]\n
-
+my $USAGE="\nUsage: $0 [-h] [-s subject] [-r subsubject] [-t type] [-d difficulty]\n
 
 ** This script creates a qa entry for a quiz library yaml file.
 
+   Example: $0 -s Linux -r bash -t provide -d moderate >> newqa
+            vi newqa
+     (then substitute \"fill in question text here\" and
+      \"fill in answer text here\" in newqa and add the file to your library)
    \n";
 
 
 my %args;
-getopts('s:r:t:d:', \%args);
+getopts('hs:r:t:d:', \%args);
 
+if (defined($args{h})) {
+  print $USAGE;
+  exit 1;
+}
 if (defined($args{s})) {
   $subject= $args{s};
 }
